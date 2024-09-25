ceea30b461157e45863c6943c3cffe90ca09af47
enoughLev <levchik-z@mail.ru>
2024-09-25 14:52:58 +0700

    25.09

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,29 @@
 package org.example;
 
+import javax.xml.transform.sax.SAXSource;
+import java.awt.*;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-
+/// 1
         int variable_1 = 1;
         while (variable_1 <= 500) {
             System.out.println(iterating(variable_1));
             variable_1 += 1;
         }
 
+/// 2
+        String not_reverse_word = "make install";
+        int lenght = not_reverse_word.length();
+        System.out.println(reverse(not_reverse_word, lenght));
 
+/// 3
+        int a = 5; int b = 3; int c = -2;
+        System.out.println(Arrays.toString(quadratic(a, b, c)));
     }
 
+/// 1
     public static String iterating(int i) {
         if (i % 5 == 0 & i % 7 == 0) {
             return "fizzbuzz";
@@ -24,5 +36,39 @@ public class Main {
         }
     }
 
+/// 2
+    public static String reverse(String word, int len){
+        String new_value = "";
+        while (len-1 >= 0) {
+            new_value = new_value + word.charAt(len-1);
+            len = len - 1;
+        }
+        return (new_value);
+
+    }
+
+/// 3
+    public static double[] quadratic(int a, int b, int c) {
+        double[] roots;
+        double D = b*b - (4*a*c);
+        if (D<0) {
+            roots = new double[0];
+        }
+        else if (D==0) {
+            roots = new double[1];
+            double x = ((double) -b/(2*a));
+            roots[0] = x;
+        }
+        else {
+            roots = new double[2];
+            double DD = Math.sqrt(D);
+            double x1 = (-b - DD) / (2*a);
+            double x2 = (-b + DD) / (2*a);
+            roots[0] = x1;
+            roots[1] = x2;
+        }
+
+        return roots;
+    }
 }
 
